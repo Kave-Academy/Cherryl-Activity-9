a9f93298db59924705189651eb2d2dd4b08db37a
Cherryl - Kave Blocks <cherryl@kaveblocks.com>
2022-02-23 17:07:38 +0800

    Highlight the bold words of the following paragraph using the For Loop Methods

--- exer.js
+++ exer.js
@@ -2,7 +2,11 @@ function run() {
 
     let highLight = document.getElementsByTagName('strong');
   
+    for (let i = 0; i < highLight.length; i++) {
+        highLight[i].style.backgroundColor = "yellow";
+       }
+      }
 
 
-}
+
 
